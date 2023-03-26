b6088e0053c3ac58153b992ae2df56ad7bcefb58
Dmitry <divinedavidbrown@yandex.ru>
2023-03-26 23:42:31 +0300

    add four sliders with the logic of passing the value to label

--- SlidersRGB/ViewController.swift
+++ SlidersRGB/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction private func choosingRedColor() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: 0,
+            blue: 0,
+            alpha: 1)
+    }
+    
+    @IBAction private func сhoosingGreenColor() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
     }
 
+    @IBAction private func choosingBlueColor() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 
 }
 
